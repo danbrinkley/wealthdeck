d559b542a4e394737cf421e95a1aff6ebb7bd653
Danielle Brinkley <daniellebrinkley.fs@gmail.com>
2021-11-06 14:05:08 -0600

    Render html and do login and sig-up page css

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.Dashboard.as_view(), name="dashboard"),
+    path('', views.Home.as_view(), name="home"),
+    path('dashboard/', views.Dashboard.as_view(), name="dashboard"),
+    path('login/', views.Login.as_view(), name="login"),
+    path('signup/', views.Signup.as_view(), name="signup"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -3,5 +3,14 @@ from django.views.generic.base import TemplateView
 # Create your views here.
 
 
+class Home(TemplateView):
+    template_name="home.html"
+
 class Dashboard(TemplateView):
     template_name="dashboard.html"
+
+class Login(TemplateView):
+    template_name="login.html"
+
+class Signup(TemplateView):
+    template_name="signup.html"
