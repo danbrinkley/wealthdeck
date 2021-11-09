7e738925ded7a0ac052507dd568b6ac8041945c9
Danielle Brinkley <daniellebrinkley.fs@gmail.com>
2021-11-09 12:23:08 -0700

    create side navbar

---
+++ wealthdeck/frontend/src/components/Dashboard/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from '../NavBar'
+
+const Dashboard = () => {
+    return (
+        <div>
+            Hello
+            <NavBar />
+        </div>
+    )
+}
+
+export default Dashboard
